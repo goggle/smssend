8a300b09819ba502e4309230de327212c64707f1
Alex Seiler <seileralex@gmail.com>
2013-09-26 17:16:59 +0200

    sendSms method

--- SmsConnect.py
+++ SmsConnect.py
@@ -2,6 +2,8 @@ import requests
 import json
 import hashlib
 import time
+import datetime
+from Phonebook import Contact
 
 class SmsConnect:
     def __init__(self):
@@ -14,6 +16,12 @@ class SmsConnect:
         self.returntype = 'json'
         self.timestamp = ''
         self.authcode = ''
+
+    def boolToInt(self, boolval):
+        if boolval:
+            return 1
+        else:
+            return 0
 
 
     def getTimestamp(self):
@@ -62,7 +70,7 @@ class SmsConnect:
         else: # TODO: error handling
             return
 
-    def calculateCredits(self, message, recipients_count):
+    def calculateCredits(self, message, recipients_count, fast=False):
         """Calculate how many credits you need to send a message. (Returns an integer).
 
         Keyword arguments:
@@ -82,7 +90,7 @@ class SmsConnect:
                 'recipients_count':recipients_count,
                 'content':message,
                 'encoding':'UTF-8',
-                'fast':0,
+                'fast':self.boolToInt(fast),
                 'returntype':self.returntype
                 }
         r = requests.post(self.url, payload)
@@ -115,7 +123,77 @@ class SmsConnect:
 #                'authcode':self.authcode,
 #                'section':self.section,
 
-    def sendSms(self, message):
-        return
+
+    def sendSms(self, recipients, message, flash=False, fast=False, schedule=False,
+            timesend=datetime.datetime.today()):
+        """Sends a sms to all the specified recipents.
+
+        Keywort arguments:
+        recipients -- a list of contact objects
+        message -- the message text as a string
+        flash -- enable sending the message in flash mode (boolean)
+        fast -- enable sending the message in fast mode (boolean)
+        schedule -- enable message scheduling (boolean)
+        timesend -- a datetime object that indicates, when the message should be send
+
+
+        """
+        self.timestamp = self.getTimestamp()
+        self.authcode = self.getAuthcode(self.timestamp, self.apikey)
+
+        recipients_numbers = []
+        for contact in recipients:
+            recipients_numbers.append(contact.getPhonenumber())
+
+        payload = {'version':self.version,
+                'timestamp':self.timestamp,
+                'username':self.username,
+                'authcode':self.authcode,
+                'section':self.section,
+                'action':'sendSMS',
+                'recipients':recipients_numbers,
+                'content':message,
+                'encoding':'UTF-8',
+                'flash':self.boolToInt(flash),
+                'fast':self.boolToInt(fast),
+                'returntype':self.returntype
+                }
+        if schedule:
+            year = str(timesend.year)
+            month = str(timesend.month)
+            if len(month) == 1:
+                month = '0' + month
+            day = str(timesend.day)
+            if len(day) == 1:
+                day = '0' + day
+            hour = str(timesend.hour)
+            if len(hour) == 1:
+                hour = '0' + hour
+            minute = str(timesend.minute)
+            if len(minute) == 1:
+                minute = '0' + minute
+            timesend_string = year + '-' + month + '-' + day + ' ' + hour + ':' + minute
+            payload.update({'schedule':1, 'timesend':timesend_string})
+
+        r = requests.post(self.url, payload)
+
+        print(payload)
+
+
+    def splitmessage(self, message):
+        """If a message is longer than 3 sms, we need to split it into several messages.
+        This method returns a list of splitted sms.
+        IMPORTANT: We assume, that the message contains no special characters!
+
+        """
+        msg_list = []
+        max_len = 459
+        msg = message
+        while len(msg) > 0:
+            msg_list.append(msg[:max_len])
+            msg = msg[max_len:]
+        return msg_list
+
+        
 
 
